79b5df24baeb1a2f05dbd169179568dbf982fac6
Emily Mearns <mearnsem@gmail.com>
2016-07-08 10:59:08 -0600

    Cells populate, sections arranged
    Need to figure out how to randomize the array

--- PairRandomizer/PairRandomizer/PairTableViewController.swift
+++ PairRandomizer/PairRandomizer/PairTableViewController.swift
@@ -10,30 +10,49 @@ import UIKit
 
 class PairTableViewController: UITableViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        tableView.reloadData()
     }
 
     @IBAction func addPersonButton(sender: AnyObject) {
+        var personTextField: UITextField?
+        
         let alertController = UIAlertController(title: "Add Person", message: "Add a person to your list", preferredStyle: .Alert)
         alertController.addTextFieldWithConfigurationHandler { (textField) in
             textField.placeholder = "Name"
+            personTextField = textField
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         alertController.addAction(cancelAction)
         
-        let addAction = UIAlertAction(title: "Add", style: .Default, handler: nil)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (_) in
+            guard let name = personTextField?.text else {return}
+            PersonController.sharedController.createPerson(name)
+            self.tableView.reloadData()
+            print("People: \(PersonController.sharedController.people.count)")
+        }
         alertController.addAction(addAction)
-        
         self.presentViewController(alertController, animated: true, completion: nil)
     }
-    // MARK: - Table view data source
     
-    // TODO: - Fix table view data source functions
+    @IBAction func randomizeButton(sender: AnyObject) {
+        PersonController.sharedController.people.shuffle()
+        print("Randomized")
+    }
+    
+    // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 4
+        return PersonController.sharedController.people.count / 2
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,27 +60,31 @@ class PairTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Header"
+        return "Group \(section + 1)"
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 25
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("personCell", forIndexPath: indexPath)
 
-        
+        let person = PersonController.sharedController.people[indexPath.row]
+        cell.textLabel?.text = person.name
 
         return cell
     }
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
+            let person = PersonController.sharedController.people[indexPath.row]
+            PersonController.sharedController.removePerson(person)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
 }
--- PairRandomizer/PairRandomizer/PersonController.swift
+++ PairRandomizer/PairRandomizer/PersonController.swift
@@ -10,6 +10,43 @@ import Foundation
 
 class PersonController {
     
+    private let keyPeople = "storedPeople"
+    static let sharedController = PersonController()
     
+    var people = [Person]()
     
+    init() {
+        loadFromPersistentStorage()
+    }
+    
+    func createPerson(name: String) {
+        let person = Person(name: name)
+        people.append(person)
+        saveToPersistentStorage()
+    }
+    
+    func removePerson(person: Person) {
+        guard let index = people.indexOf(person) else {return}
+        people.removeAtIndex(index)
+        saveToPersistentStorage()
+    }
+    
+    func loadFromPersistentStorage() {
+        guard let peopleDictionaries = NSUserDefaults.standardUserDefaults().objectForKey(keyPeople) as? [[String: AnyObject]] else {return}
+        people = peopleDictionaries.flatMap({Person(dictionary: $0)})
+    }
+    
+    func saveToPersistentStorage() {
+        NSUserDefaults.standardUserDefaults().setObject(people.map({$0.dictionaryCopy}), forKey: keyPeople)
+    }
+}
+
+extension Array {
+    mutating func shuffle() {
+        for _ in 0..<10 {
+            sortInPlace({ (_, _) -> Bool in
+                arc4random() < arc4random()
+            })
+        }
+    }
 }
